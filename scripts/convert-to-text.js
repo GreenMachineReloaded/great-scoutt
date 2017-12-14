229cf2071fe42f70d579854ad087e0503ee6d428
Jeremy School <jschool504@gmail.com>
2017-12-14 18:18:29 -0500

    added script file

---
+++ scripts/convert-to-text.js
@@ -0,0 +1,4 @@
+// 1. get path to matches.json file
+// 2. load json
+// 3. for each match, create a formatted string representating it
+// 4. output these into seperate documents in a target directory
